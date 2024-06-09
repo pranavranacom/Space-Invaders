5c44934465d602b4abc401419d5f4379eb542707
Pranav Rana <prnvrna@gmail.com>
2024-06-09 14:02:05 +0530

    Initial shapes
    This one creates initial shapes of circle, square and triangle.

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -18,9 +18,31 @@ int main() {
         }
 
         // Clear the window
-        window->clear(sf::Color::Blue);
+        window->clear(sf::Color::Cyan);
 
         // Draw your content here...
+        /* A Green circle */
+        sf::CircleShape circle(50); // Radius 50
+        circle.setFillColor(sf::Color::Green);
+        circle.setPosition(10, 10); // Set position
+        window->draw(circle);
+
+        // A Red square
+        sf::RectangleShape square(sf::Vector2f(100, 100)); // 100x100 pixels
+        square.setPosition(150, 10); // Center of the window (approximately)
+        square.setFillColor(sf::Color::Red);
+        window->draw(square);
+
+        // A Blue Triangle
+        sf::ConvexShape triangle;
+        triangle.setPointCount(3);
+        //// points are set as a pendulum: top to bottom left to right
+        triangle.setPoint(0, sf::Vector2f(0, 100));
+        triangle.setPoint(1, sf::Vector2f(50, 0));
+        triangle.setPoint(2, sf::Vector2f(100, 100));
+        triangle.setFillColor(sf::Color::Blue);
+        triangle.setPosition(300, 10); // Position the origin of the triangle at (400, 300)
+        window->draw(triangle);
 
         // Display what was drawn
         window->display();
