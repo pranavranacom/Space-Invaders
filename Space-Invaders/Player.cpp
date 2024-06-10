eb269b78cacf39b83a8ac12eb637bd5198336afa
Pranav Rana <pranavrana.com@gmail.com>
2024-06-10 20:04:35 +0530

    Added Player Properties and Methods
    Added private, and public properties, getters and setters for the Player class.

---
+++ Space-Invaders/Player.cpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class Player {
+
+private:
+	int health = 3;
+	sf::Vector2f position = sf::Vector2f(200.0f, 100.0f);
+	int movement_speed = 5;
+	int player_score = 0;
+
+public:
+	sf::Texture player_texture;
+	sf::Sprite player_sprite;
+
+	// Public Getter & Setter methods
+	int getScore() {
+		return player_score;
+	};
+
+	void setScore(int newScore) {
+		player_score = newScore;
+	};
+
+	// New methods
+	void takeDamage() {};
+	void move() {};
+	void shootBullets() {};
+};
--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,33 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Player.h"
 
 int main()
 {
+    // Define the video mode (dimensions)
+    sf::VideoMode videoMode = sf::VideoMode(800, 600);
+
+    // Create a window object with specific dimensions and a title
+    sf::RenderWindow window(videoMode, "SFML Window");
+
+    // the Player class
+    Player player;
+    // std::cout << player.movement_speed << "\\n";
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Check for window closure
+            if (event.type == sf::Event::Closed)  window.close();
+        }
+
+
+        // Clear the window
+        window.clear(sf::Color::Magenta);
+
+        // Display whatever you draw
+        window.display();
+    }
+
     return 0;
 }
