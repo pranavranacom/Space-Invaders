566a5a5652b83bfe86b1d027299f9470c8bbd869
Pranav Rana <pranavrana.com@gmail.com>
2024-06-16 12:47:26 +0530

    Added Service Locator Class
    1. Initialized ServiceLocator class.
    2. Created .h, and .cpp files.
    3. Consists of the startup code.

---
+++ Space-Invaders/Source/ServiceLocator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "../Header/ServiceLocator.h"
+
+ServiceLocator::ServiceLocator(){}
+ServiceLocator::~ServiceLocator() {}
+
+void ServiceLocator::createServices(){}
+void ServiceLocator::clearAllServices(){}
+
+static ServiceLocator* getInstance(){}
+
+void ServiceLocator::initialize(){}
+void ServiceLocator::update(){}
+void ServiceLocator::render(){}
